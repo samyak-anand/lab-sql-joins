d6c1714b41aefef9e84b97637284487cfdcaa7ab
Samyak Anand <samyak.anand@gmail.com>
2025-05-05 16:11:45 +0200

    lab solved

--- join_lab.sql
+++ join_lab.sql
@@ -41,3 +41,25 @@ from film f
 inner join film_category fc on f.film_id= fc.film_id
 inner join category c on c.category_id= fc.category_id
 group by(c.name);
+
+/*
+Identify the film categories with the longest average running time.
+Display the top 10 most frequently rented movies in descending order.
+Determine if "Academy Dinosaur" can be rented from Store 1.
+Provide a list of all distinct film titles, along with their availability status in the inventory. 
+Include a column indicating whether each title is 'Available' or 'NOT available.' Note that there are 42 titles that are not in the inventory, 
+and this information can be obtained using a CASE statement combined with IFNULL."
+*/
+
+#Identify the film categories with the longest average running time
+select c.name, avg(f.length) as avg_running_time
+from film f  
+inner join film_category fc on f.film_id= fc.film_id
+inner join category c on c.category_id= fc.category_id
+group by(c.name)
+order by avg_running_time DESC;
+
+#Display the top 10 most frequently rented movies in descending order.
+
+
+#select * from rental
